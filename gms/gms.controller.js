dc0c61455984ea45eb6bcf90f7f56455bbd08cda
Ram Vinoth Ponnarasu <ramvinoth37@gmail.com>
2020-11-18 16:45:51 -0800

    first commit

---
+++ gms/gms.controller.js
@@ -0,0 +1,18 @@
+﻿const express = require('express');
+const router = express.Router();
+// const userService = require('./user.service');
+
+// routes
+router.post('/rest/thirdparty/cppm/identityInfo', logPostReqFromCPPM);
+router.get('/', getAll);
+
+module.exports = router;
+
+function logPostReqFromCPPM(req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
+}
+
+function getAll(req, res, next) {
+    return res.json({"result": "Authenticated"});
+}
